6c6053611495b97680d19b1aa4bd50716439155f
Burak Kızılay <burak.kizilay@westerops.com>
2025-01-14 14:40:07 +0300

    feat: add home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,80 @@
 import Image from "next/image";
+import { ReactNode } from "react";
+
+const IMAGE_URL = "https://avatars.githubusercontent.com/u/40871181?v=4";
+
+const Profile = () => (
+  <div className="flex items-center gap-4 ">
+    <Image
+      src={IMAGE_URL}
+      alt="Profile Picture"
+      width={60}
+      height={60}
+      className="rounded-full"
+    />
+    <h1 className="text-5xl font-bold">Burak Kızılay</h1>
+  </div>
+);
+
+const Introduction = () => (
+  <p className="text-gray-400">
+    Focused on creating mobile applications that users will want to use and
+    enjoy.
+    <br />
+    <span className="text-white text-xl font-semibold">
+      Based in Osmaniye, Türkiye
+    </span>
+  </p>
+);
+
+const Section = ({ title, content }: { title: string; content: ReactNode }) => (
+  <section className="flex flex-col gap-2">
+    <p className="text-gray-500 uppercase font-medium">{title}</p>
+    {content}
+  </section>
+);
+
+const Links = () => {
+  const links = [
+    { href: "mailto:kizilay4696@gmail.com", label: "Email" },
+    { href: "https://x.com/devburakkizilay", label: "X" },
+    { href: "https://github.com/burakkizilay46", label: "Github" },
+    { href: "https://www.linkedin.com/in/burak-kizilay/", label: "LinkedIn" },
+  ];
+
+  return (
+    <div className="flex gap-4">
+      {links.map(({ href, label }) => (
+        <a key={href} href={href} className="underline  hover:text-gray-600">
+          {label}
+        </a>
+      ))}
+    </div>
+  );
+};
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
+    <div className="w-full h-screen flex">
+      <div className="m-auto flex flex-col gap-6 text-lg">
+        <Profile />
+        <Introduction />
+        <Section
+          title="Currently"
+          content={
+            <p>
+              Software Engineer / Mobile and Frontend Developer at{" "}
+              <a
+                href="https://www.westerops.com"
+                className="underline  hover:text-gray-600"
+              >
+                Westerops
+              </a>
+            </p>
+          }
         />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        <Section title="Elsewhere" content={<Links />} />
+      </div>
     </div>
   );
 }
---
+++ app/test-page/page.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const TestPage = () => {
+  return (
+    <div className="m-auto">
+      <p className="title">Title</p>
+      <p className="subtitle">Subtitle</p>
+      <p className="section-title">Section Title</p>
+      <p className="content-title">Content Title</p>
+      <p className="content">Content</p>
+      <p className="content-sm">Content Small</p>
+      <p className="content-xs">Content XS</p>
+      <p className="label">Label</p>
+      <p className="label-sm">Label Small</p>
+      <p className="label-xs">Label XS</p>
+      <a className="link" href="https://google.com">
+        Link Text
+      </a>
+    </div>
+  );
+};
+
+export default TestPage;
